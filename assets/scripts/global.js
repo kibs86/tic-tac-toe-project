512def55d5d73e03127fe19fb63030a03f2d8f48
kibs86 <kibs086@gmail.com>
2016-11-01 14:36:36 -0400

    Add comments to clarify code
    Add a few comments in global.js and index.js

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// create an object to store items I'm using as global variables
 const globalVars = {
   playerLogin: false,
   createGameSuccess: false,
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,11 +4,13 @@ const authEvents = require('./auth/events.js');
 const gameEvents = require('./game/events.js');
 
 $(()=>{
+  // event handlers for login API
   authEvents.addHandlers();
 
-  // enable game to be played once
+  // event handlers for game API
   gameEvents.addGameHandlers();
 
+  // clear out form data if user closes form instead of hitting submit
   $(".reset").click(function() {
         $("input").val("");
     });

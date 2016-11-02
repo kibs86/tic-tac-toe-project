609694bce949106dc3c036e273149c39c0fb4dd6
kibs86 <kibs086@gmail.com>
2016-11-01 22:09:22 -0400

    implement win checks and better click handlers
    - implement win checks
    - use data-owner to update background and mark owner of tile

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -2,7 +2,7 @@
 
 const globalJS = require('./global.js');
 
-let tileArray = [];
+let tileArray = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
 const switchPlayer = function () {
   console.log('current player is ' + globalJS.globalVars.activePlayer);
@@ -13,29 +13,93 @@ const switchPlayer = function () {
   }
 };
 
-const onTileClick = function () {
-  let tileID = '#' + $(this).attr('id');
+const checkRowWins = function(tileArray) {
+  console.log(tileArray);
+  if ((tileArray[0] === tileArray[1]) && (tileArray[0] === tileArray[2])) {
+    globalJS.globalVars.gameWinner = tileArray[0];
+    return true;
+  } else if ((tileArray[3] === tileArray[4]) && (tileArray[3] === tileArray[5])) {
+      globalJS.globalVars.gameWinner = tileArray[3];
+      return true;
+    } else if ((tileArray[6] === tileArray[7]) && (tileArray[6] === tileArray[8])) {
+        globalJS.globalVars.gameWinner = tileArray[6];
+        return true;
+    } else {
+      return false;
+    }
+};
 
-  // change background of tile
+const checkColWins = function(tileArray) {
+  if ((tileArray[0] === tileArray[3]) && (tileArray[0] === tileArray[6])) {
+    globalJS.globalVars.gameWinner = tileArray[0];
+    return true;
+  } else if ((tileArray[1] === tileArray[4]) && (tileArray[1] === tileArray[7])) {
+      globalJS.globalVars.gameWinner = tileArray[1];
+      return true;
+    } else if ((tileArray[2] === tileArray[5]) && (tileArray[2] === tileArray[8])) {
+        globalJS.globalVars.gameWinner = tileArray[2];
+        return true;
+    } else {
+      return false;
+    }
+};
+
+const checkDiagWins = function(tileArray) {
+  if ((tileArray[0] === tileArray[4]) && (tileArray[0] === tileArray[8])) {
+    globalJS.globalVars.gameWinner = tileArray[0];
+    return true;
+  } else if ((tileArray[2] === tileArray[4]) && (tileArray[2] === tileArray[6])) {
+      globalJS.globalVars.gameWinner = tileArray[2];
+      return true;
+    }
+};
+
+const setOwner = function(tileID, tileIndex) {
   if (globalJS.globalVars.activePlayer === 'x') {
-    $(tileID).addClass('x-background');
+    $(tileID).attr('data-owner', 'x');
+    tileArray[tileIndex] = 'x';
   } else {
-    $(tileID).addClass('o-background');
+    $(tileID).attr('data-owner', 'o');
+    tileArray[tileIndex] = 'o';
   }
 
-  // disable handler
+  return tileArray;
+
+};
+
+const onTileClick = function () {
+  // find ID and index of tile that was clicked
+  let tile = $(this).attr('id');
+  let tileID = '#' + tile;
+  let tileIndex = tile[tile.length - 1];
+
+  // increment turn count. need to test that this is in the right place
+  globalJS.globalVars.turnCount++;
+
+  // change background of tile and store owner
+  tileArray = setOwner(tileID, tileIndex);
+
+  // check for a winner - need to get it to print winner in correct location
+  if (checkColWins(tileArray) || checkRowWins(tileArray) || checkDiagWins(tileArray)) {
+    if (globalJS.globalVars.gameWinner === 'x') {
+      //$('.player1-message').text("CONGRATULATIONS! YOU WON IN " + globalJS.globalVars.turnCount + " TURNS!");
+      $('.player1-message').text("YOU WON IN " + globalJS.globalVars.turnCount + " TURNS!");
+    } else {
+      $('.player2-message').html('CONGRATULATIONS!<br>YOU WON IN globalJS.globalVars.turnCount TURNS!');
+    }
+  }
+
+  // disable handler for tile that was just clicked
   $(tileID).css("pointer-events", "none");
 
-  // update player
+  // update active player
   switchPlayer();
 
-  // find out which player clicked
-  // get array index from class of tile clicked
-  // put x or o into correct index
-  // change background image of tile
-  // disable handler
-  // increment turn count
-  // update player
+  // find out who won
+  // disable all click events if there's a winner
+  // update game API
+  // new game button
+    // global variables, player messages, game array, create game API
 };
 
 // win check
--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -1,11 +1,15 @@
 'use strict';
 
 // create an object to store items I'm using as global variables
+// throw debug statement in right after New Game handler to check values
+// of below variables and figure out which ones to reset
 const globalVars = {
   playerLogin: false,
   createGameSuccess: false,
   newestGameID: 0,
-  activePlayer: 'x'
+  activePlayer: 'x',
+  gameWinner: '',
+  turnCount: 0
 };
 
 module.exports = {

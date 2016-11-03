c2d7d27b70ea3b235f6b4725d8bfc5eac7f5a9a8
kibs86 <kibs086@gmail.com>
2016-11-03 15:26:53 -0400

    Re-deploy to see if new merge master worked

--- bundle.js
+++ bundle.js
@@ -248,7 +248,7 @@ webpackJsonp([0],[
 	'use strict';
 
 	var config = {
-	  host: 'https://aqueous-atoll-85096.herokuapp.com/'
+	  host: 'https://aqueous-atoll-85096.herokuapp.com'
 	};
 
 	module.exports = config;
